bcaaba42833e098c22c025ccfc090cb139dcac41
Allen <64094914+allendema@users.noreply.github.com>
2026-01-26 23:07:15 +0100

    fix: sound applet: this._seeker is undefined
    ~$ less ~/.xsession-errors
    #
    (cinnamon:1792): Gjs-WARNING **: 19:59:42.252: JS ERROR: Exception in callback for signal: NameOwnerChanged: TypeError: this._seeker is undefined
    destroy@/usr/share/cinnamon/applets/sound@cinnamon.org/applet.js:923:9
    _removePlayer@/usr/share/cinnamon/applets/sound@cinnamon.org/applet.js:1470:34
    CinnamonSoundApplet/</this._ownerChangedId<@/usr/share/cinnamon/applets/sound@cinnamon.org/applet.js:1024:34
    _callHandlers@resource:///org/gnome/gjs/modules/core/_signals.js:130:42
    _emit@resource:///org/gnome/gjs/modules/core/_signals.js:119:10
    _convertToNativeSignal@resource:///org/gnome/gjs/modules/core/overrides/Gio.js:152:19
    
    Signed-off-by: Allen <64094914+allendema@users.noreply.github.com>

--- files/usr/share/cinnamon/applets/sound@cinnamon.org/applet.js
+++ files/usr/share/cinnamon/applets/sound@cinnamon.org/applet.js
@@ -918,7 +918,9 @@ class Player extends PopupMenu.PopupMenuSection {
     }
 
     destroy() {
-        this._seeker.destroy();
+        if (this._seeker)
+            this._seeker.destroy();
+        }
         if (this._prop)
             this._prop.disconnectSignal(this._propChangedId);
 
